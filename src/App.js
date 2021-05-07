dd4e537f7fb555600a262a46f2e71bf7eb239c8e
chirptotheeast <alyssa.easterly@yahoo.com>
2021-05-07 19:45:14 -0400

    search bar rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-// import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar'
 
 function App() {
   return (
     <div className="App">
       Hello, welcome to the best Shopify Site in town.
-    {/* <SearchBar /> */}
+    <SearchBar />
     </div>
   );
 }
